90a1955ba90f42acc8f93f2459119fc16170385c
Kyle Moseby <kyle@kylemoseby.com>
2016-07-28 05:24:42 -0700

    New source doc

--- source/angular-flickr-integrations.js
+++ source/angular-flickr-integrations.js
@@ -1,3 +1,79 @@
+// (function() {
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name mkm.flickr.restAPI
+ * @description
+ * # restAPI
+ * Service in the mkm.flickr.
+ */
+angular.module('mkm.flickr', [
+    'ngRoute',
+    'API',
+    'wu.masonry'
+  ])
+  .service('restAPI', ['$http', '$api', function($http, $api) {
+
+    var apiKey = $api.flickr;
+
+    function _getPhotoset(setid) {
+
+      return $http
+        .jsonp('https://api.flickr.com/services/rest/', {
+          cache: false,
+          params: {
+            'jsoncallback': 'JSON_CALLBACK',
+            'method': 'flickr.photosets.getPhotos',
+            'api_key': apiKey,
+            'photoset_id': setid,
+            'extras': 'description',
+            'format': 'json'
+          }
+        });
+    }
+
+    function _getRecent(userid) {
+
+      return $http
+        .jsonp('https://api.flickr.com/services/rest/', {
+          cache: false,
+          params: {
+            'jsoncallback': 'JSON_CALLBACK',
+            'method': 'flickr.people.getPhotos',
+            'user_id': userid,
+            'api_key': apiKey,
+            'extras': 'description',
+            'format': 'json',
+            'per_page': 500
+          }
+        });
+    }
+
+    function _getImg(imgid) {
+
+      return $http
+        .jsonp('https://api.flickr.com/services/rest/', {
+          cache: false,
+          params: {
+            'jsoncallback': 'JSON_CALLBACK',
+            'method': 'flickr.photos.getInfo',
+            'api_key': apiKey,
+            'photo_id': imgid,
+            'format': 'json'
+          }
+        });
+    }
+
+    return {
+      getPhotoset: _getPhotoset,
+      getRecent: _getRecent,
+      getImg: _getImg
+    };
+
+  }]);
+// }());
+
 'use strict';
 
 /**
@@ -232,79 +308,3 @@ angular.module('mkm.flickr')
       }
     };
   }]);
-
-// (function() {
-'use strict';
-
-/**
- * @ngdoc service
- * @name mkm.flickr.restAPI
- * @description
- * # restAPI
- * Service in the mkm.flickr.
- */
-angular.module('mkm.flickr', [
-    'ngRoute',
-    'API',
-    'wu.masonry'
-  ])
-  .service('restAPI', ['$http', '$api', function($http, $api) {
-
-    var apiKey = $api.flickr;
-
-    function _getPhotoset(setid) {
-
-      return $http
-        .jsonp('https://api.flickr.com/services/rest/', {
-          cache: false,
-          params: {
-            'jsoncallback': 'JSON_CALLBACK',
-            'method': 'flickr.photosets.getPhotos',
-            'api_key': apiKey,
-            'photoset_id': setid,
-            'extras': 'description',
-            'format': 'json'
-          }
-        });
-    }
-
-    function _getRecent(userid) {
-
-      return $http
-        .jsonp('https://api.flickr.com/services/rest/', {
-          cache: false,
-          params: {
-            'jsoncallback': 'JSON_CALLBACK',
-            'method': 'flickr.people.getPhotos',
-            'user_id': userid,
-            'api_key': apiKey,
-            'extras': 'description',
-            'format': 'json',
-            'per_page': 500
-          }
-        });
-    }
-
-    function _getImg(imgid) {
-
-      return $http
-        .jsonp('https://api.flickr.com/services/rest/', {
-          cache: false,
-          params: {
-            'jsoncallback': 'JSON_CALLBACK',
-            'method': 'flickr.photos.getInfo',
-            'api_key': apiKey,
-            'photo_id': imgid,
-            'format': 'json'
-          }
-        });
-    }
-
-    return {
-      getPhotoset: _getPhotoset,
-      getRecent: _getRecent,
-      getImg: _getImg
-    };
-
-  }]);
-// }());
