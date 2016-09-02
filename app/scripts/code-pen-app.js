a772c586f166ef217a023b2a887200fdadc35729
Kyle Moseby <kylemoseby@users.noreply.github.com>
2016-09-01 19:48:00 -0700

    Checking codepen

--- app/scripts/code-pen-app.js
+++ app/scripts/code-pen-app.js
@@ -2,11 +2,14 @@
 
 angular
   .module('demoApp', [
+    'ngMaterial'
     'ngMaterial',
     'mkm.flickr',
   ])
   .controller('MainCtrl', ['$scope', '$http', function($scope) {
-
+    
+    console.log('codepen controller');
+    
     $scope.albumID = '72157642607219393';
 
     $scope.flickrID = '91631856@N00';
